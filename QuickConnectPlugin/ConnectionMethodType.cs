0ddf21a464e7d88006f4e02d6260e593f3f34ba9
Cristian Stoica <cristianstoica85@gmail.com>
2016-02-25 02:05:33 +0200

    Fixed ConnectionMethodType enums.

--- QuickConnectPlugin/ConnectionMethodType.cs
+++ QuickConnectPlugin/ConnectionMethodType.cs
@@ -5,9 +5,9 @@ namespace QuickConnectPlugin {
     public enum ConnectionMethodType {
 
         Unknown = 0,
-        RemoteDesktop = 2,
-        RemoteDesktopConsole = 3,
+        RemoteDesktop = 1,
+        RemoteDesktopConsole = 2,
+        vSphereClient = 3,
         PuttySSH = 4,
-        vSphereClient = 5,
     }
 }
